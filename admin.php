6482c13402bd810288511fe2f895a3ea3235f14a
WP-Cat.net <113475061+hybernia1@users.noreply.github.com>
2025-10-17 20:41:00 +0200

    Improve admin access guards

--- admin.php
+++ admin.php
@@ -5,6 +5,8 @@ use Cms\Http\AdminController;
 use Cms\Http\AdminAuthController;
 use Cms\Auth\AuthService;
 use Cms\Auth\Authorization;
+use Cms\Utils\AdminNavigation;
+use Cms\View\ViewEngine;
 
 require_once __DIR__ . '/load.php';
 
@@ -25,12 +27,28 @@ if ($route === 'auth') {
 $auth = new AuthService();
 $user = $auth->user();
 if (!$user) {
-    cms_redirect_to_front_login();
+    (new AdminAuthController())->handle('login');
+    exit;
 }
 
 // >>> ROLE GUARD: pouze admin <<<
 if (!Authorization::isAdmin($user)) {
-    cms_redirect_to_front_login();
+    http_response_code(403);
+
+    $flash = null;
+    if (isset($_SESSION['_flash']) && is_array($_SESSION['_flash'])) {
+        $flash = $_SESSION['_flash'];
+    }
+    unset($_SESSION['_flash']);
+
+    $view = new ViewEngine(__DIR__ . '/admin/views');
+    $view->render('errors/403', [
+        'pageTitle'   => '403 – Přístup odepřen',
+        'nav'         => AdminNavigation::build('dashboard'),
+        'currentUser' => $user,
+        'flash'       => $flash,
+    ]);
+    exit;
 }
 
 $controller = new AdminController(baseViewsPath: __DIR__ . '/admin/views');
