dc9b63ff0fa1d5b7265f806dc5a700827a8b10fd
WP-Cat.net <113475061+hybernia1@users.noreply.github.com>
2025-10-17 03:00:15 +0200

    Move shared templates under inc

--- themes/classic/templates/search.php
+++ themes/classic/templates/search.php
@@ -2,16 +2,17 @@
 /** @var array<int,array<string,mixed>> $items */
 /** @var string $query */
 /** @var \Cms\Utils\LinkGenerator $urls */
-
-$h = static fn(string $value): string => htmlspecialchars($value, ENT_QUOTES, 'UTF-8');
 ?>
 <section class="card card--section">
   <header class="card__header card__header--stacked">
     <h1 class="card__title">Hledání</h1>
-    <form class="search-form" method="get" action="<?= $h($urls->search()) ?>">
-      <input type="text" class="form-field__control" name="s" value="<?= $h($query) ?>" placeholder="Co hledáte?">
-      <button class="btn btn--primary">Hledat</button>
-    </form>
+    <?php $this->part('parts/search/form', [
+      'query'   => $query,
+      'action'  => $urls->search(),
+      'classes' => [
+        'input' => 'form-field__control',
+      ],
+    ]); ?>
   </header>
   <?php $this->part('parts/post-list', [
     'posts'        => $items,
